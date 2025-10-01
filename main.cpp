4e8a0cb00a84f8ed7a6db99bf58c7b4e20c86c44
Gavin O'Connor <gavinfrancisoconnor@gmail.com>
2025-09-30 23:37:39 -0700

    reimplemented a lot of the enums for parsing, got a solid start on nud

--- main.cpp
+++ main.cpp
@@ -1,18 +1,165 @@
 #include <regex>
 #include <vector>
+#include <variant>
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include <utility>
+#include <cctype>
+
+int colLetterToNumber(const std::string &col)
+{
+    int result = 0;
+    for (char c : col)
+    {
+        if (!std::isalpha(c))
+            continue; // skip non-letters if you want safety
+        char upper = std::toupper(c);
+        int digit = (upper - 'A' + 1); // A=1, B=2, ..., Z=26
+        result = result * 26 + digit;
+    }
+    return result;
+}
+
+// Add,
+//     Sub,
+//     Mul,
+//     Div,
+//     Pow,
+//     Less,
+//     Greater,
+//     Eq,
+//     Neq,
+//     Leq,
+//     Geq,
+//     Range,  // :
+//     Concat, // &
 
 enum TOKEN_TYPE
 {
-    REFERENCE,
-    NUMBER, // done
-    STRING, // done
-    COMMA,  // done
-    OPERATOR,
-    IDENT,
-    LPAREN, // done
-    RPAREN, // done
+    REFERENCE_TOKEN,
+    NUMBER_TOKEN,
+    STRING_TOKEN,
+    COMMA_TOKEN,
+    ADD_OPERATOR_TOKEN,
+    SUB_OPERATOR_TOKEN,
+    MULT_OPERATOR_TOKEN,
+    DIV_OPERATOR_TOKEN,
+    POW_OPERATOR_TOKEN,
+    LESS_OPERATOR_TOKEN,
+    GREATER_OPERATOR_TOKEN,
+    EQ_OPERATOR_TOKEN,
+    NEQ_OPERATOR_TOKEN,
+    GEQ_OPERATOR_TOKEN,
+    LEQ_OPERATOR_TOKEN,
+    RANGE_OPERATOR_TOKEN,
+    CONCAT_OPERATOR_TOKEN,
+    PERCENT_OPERATOR_TOKEN,
+    IDENT_TOKEN,
+    LPAREN_TOKEN, // done
+    RPAREN_TOKEN, // done
+};
+
+// need literal, function call, operator, reference
+
+enum class LiteralType
+{
+    String,
+    Numeric,
+};
+
+enum class UnaryOp
+{
+    Plus,    // +x
+    Minus,   // -x
+    Percent, // x%
+};
+
+enum class BinaryOp
+{
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+    Less,
+    Greater,
+    Eq,
+    Neq,
+    Leq,
+    Geq,
+    Range,  // :
+    Concat, // &
+};
+
+enum class ReferenceType
+{
+    Cell,
+    Range,
+};
+
+enum class ASTNodeType
+{
+    Literal,
+    Reference,
+    Unary,
+    Binary,
+    FunctionCall,
+};
+
+// === Leaf payloads ===
+struct Literal
+{
+    LiteralType type;
+    std::variant<std::string, double> value;
+};
+
+struct CellReference
+{
+    int row;
+    int col;
+    // add absolute flags later if needed
+};
+
+struct RangeReference
+{
+    int top;
+    int left;
+    int bottom;
+    int right;
+};
+
+struct Reference
+{
+    ReferenceType type;
+    std::variant<CellReference, RangeReference> ref;
+};
+
+struct ASTNode;
+
+struct UnaryOperation
+{
+    UnaryOp op;
+    ASTNode *operand;
+};
+
+struct BinaryOperation
+{
+    BinaryOp op;
+    ASTNode *left;
+    ASTNode *right;
+};
+
+struct FunctionCall
+{
+    std::string identifier;
+    std::vector<ASTNode *> args;
+};
+
+struct ASTNode
+{
+    ASTNodeType type;
+    std::variant<Literal, Reference, UnaryOperation, BinaryOperation, FunctionCall> node;
 };
 
 struct Token
@@ -22,12 +169,70 @@ struct Token
     std::string token;
 };
 
-std::string valid_after_number = ",)+-/*^";
+ASTNode nud(Token token)
+{
+    ASTNode node;
+    Literal lit;
+    Reference ref;
+    CellReference cell_ref;
+    std::string cell_ref_letters;
+    int cell_ref_numbers = 0;
+    switch (token.type)
+    {
+    case NUMBER_TOKEN:
+        lit.type = LiteralType::Numeric;
+        lit.value = std::stod(token.token);
+        node.type = ASTNodeType::Literal;
+        node.node = std::move(lit);
+        return node;
+    case STRING_TOKEN:
+        lit.type = LiteralType::String;
+        lit.value = std::move(token.token);
+        node.type = ASTNodeType::Literal;
+        node.node = std::move(lit);
+        return node;
+    case REFERENCE_TOKEN:
+        ref.type = ReferenceType::Cell;
+
+        for (int i = 0; i < token.token.size(); i++)
+        {
+            // 0 shouldn't be allowed in cell ref but this logic clips it
+            if (isdigit(token.token[i]))
+            {
+                cell_ref_numbers *= 10;
+                cell_ref_letters += std::stoi("" + token.token[i]);
+            }
+            else
+            {
+                cell_ref_letters.push_back(token.token[i]);
+            }
+        }
+        cell_ref.row = cell_ref_numbers;
+        cell_ref.col = colLetterToNumber(cell_ref_letters);
+        ref.ref = std::move(cell_ref);
+        node.type = ASTNodeType::Reference;
+        node.node = std::move(ref);
+    case LPAREN_TOKEN:
+        static_assert(1 == 2);
+        // continue parsing
+    case ADD_OPERATOR_TOKEN:
+        static_assert(1 == 2);
+        // parse next sub expression and wrap
+    case SUB_OPERATOR_TOKEN:
+        static_assert(1 == 2);
+        // parse next sub expression and wrap
+    default:
+        throw std::runtime_error("PARSING ERROR");
+    }
+}
+
+std::string valid_after_number = ",)+-/*^%";
 // : is not included below because we won't reach that branch with a cell ref
-std::string valid_after_indentifier = ",()+-/*^";
-std::string valid_after_reference = ",:)+-/*^";
+std::string valid_after_indentifier = ",()+-/*^:";
+std::string valid_after_reference = ",:)+-/*^>=<";
 std::string operators = "+-/*^:";
-std::string binary_math_operators = "/*^";
+std::string binary_math_operators = "/*^&";
+std::string comparison_operators = "<=>=<>";
 
 // scan based tokenizer
 
@@ -58,8 +263,7 @@ std::vector<Token> tokenize(std::string input)
                     else
                     {
                         string_closed = true;
-                        tokens.push_back({STRING, "STRING", value});
-                        i += 1;
+                        tokens.push_back({STRING_TOKEN, "STRING", value});
                         break;
                     }
                 }
@@ -85,7 +289,6 @@ std::vector<Token> tokenize(std::string input)
             while (i < input.size())
             {
                 // check if valid follower
-                //||  || (!decimal_last && (input[i] == 'e' || input[i] == 'E')))
                 if (isdigit(input[i]))
                 {
                     value.push_back(input[i]);
@@ -135,24 +338,17 @@ std::vector<Token> tokenize(std::string input)
                 {
                     if (i < input.size())
                     {
-                        if (input[i] == '%')
+                        // either end of string or valid following character
+                        if (i == input.size() - 1)
                         {
-                            // either end of string or valid following character
-                            if (i == input.size() - 1 || valid_after_number.find(input[i + 1]) != std::string::npos)
-                            {
-                                value.push_back('%');
-                                tokens.push_back({NUMBER, "NUMBER", value});
-                                token_pushed = true;
-                                break;
-                            }
-                            else
-                            {
-                                throw std::runtime_error("INVALID NUMBER ENDING");
-                            }
+                            value.push_back('%');
+                            tokens.push_back({NUMBER_TOKEN, "NUMBER", value});
+                            token_pushed = true;
+                            break;
                         }
                         else if (valid_after_number.find(input[i]) != std::string::npos)
                         {
-                            tokens.push_back({NUMBER, "NUMBER", value});
+                            tokens.push_back({NUMBER_TOKEN, "NUMBER", value});
                             token_pushed = true;
                             i--;
                             break;
@@ -162,27 +358,34 @@ std::vector<Token> tokenize(std::string input)
                             throw std::runtime_error("INVALID NUMBER ENDING");
                         }
                     }
-                    // skip whitespace then check chars for valid followers to a number
                 }
             }
             if (!token_pushed)
             {
-                tokens.push_back({NUMBER, "NUMBER", value});
+                tokens.push_back({NUMBER_TOKEN, "NUMBER", value});
                 i--;
             }
+        }
+        else if (input[i] == '%')
+        {
+            if (!tokens.size())
+                throw std::runtime_error("CAN'T START EXPRESSION WITH %");
+            if (tokens[tokens.size() - 1].type != LPAREN_TOKEN && tokens[tokens.size() - 1].type != REFERENCE_TOKEN && tokens[tokens.size() - 1].type != NUMBER_TOKEN)
+                throw std::runtime_error("INCORRECT LEFT OPERAND FOR %");
+            tokens.push_back({PERCENT_OPERATOR_TOKEN, "PERCENT OPERATOR", "%"});
         }
         else if (input[i] == ')')
         {
             if (open_parens == 0)
                 throw std::runtime_error("CLOSING PAREN WITHOUT OPEN");
-            tokens.push_back({RPAREN, "RPAREN", ")"});
+            tokens.push_back({RPAREN_TOKEN, "RPAREN", ")"});
         }
         else if (input[i] == '(')
         {
             if (i == input.size() - 1)
                 throw std::runtime_error("CANNOT END FORMULA WITH OPEN PAREN");
             open_parens++;
-            tokens.push_back({LPAREN, "LPAREN", "("});
+            tokens.push_back({LPAREN_TOKEN, "LPAREN", "("});
         }
         else if (input[i] == ',')
         {
@@ -190,21 +393,93 @@ std::vector<Token> tokenize(std::string input)
                 throw std::runtime_error("COMMAS ARE NOT VALID OUTSIDE OF AN EXPRESSION");
             if (!tokens.size())
                 throw std::runtime_error("CANNOT START FORMULA WITH COMMA");
-            if (tokens[tokens.size() - 1].type == OPERATOR || tokens[tokens.size() - 1].type == LPAREN)
+            if ((ADD_OPERATOR_TOKEN <= tokens[tokens.size() - 1].type && tokens[tokens.size() - 1].type <= PERCENT_OPERATOR_TOKEN) || tokens[tokens.size() - 1].type == LPAREN_TOKEN)
                 throw std::runtime_error("CANNOT FOLLOW OPERATOR OR LPAREN WITH COMMA");
-            tokens.push_back({COMMA, "COMMA", ","});
+            tokens.push_back({COMMA_TOKEN, "COMMA", ","});
+        }
+        else if (input[i] == '=')
+        {
+            if (i == tokens.size() - 1)
+                throw std::runtime_error("CAN'T END EXPRESSION WITH =");
+            if (!tokens.size())
+                throw std::runtime_error("CAN'T START EXPRESSION WITH =");
+            if (tokens[tokens.size() - 1].type == RPAREN_TOKEN)
+                throw std::runtime_error("INCORRECT LEFT OPERAND FOR =");
+            tokens.push_back({EQ_OPERATOR_TOKEN, "EQ OPERATOR", "="});
+        }
+        else if (input[i] == '<')
+        {
+            if (i == tokens.size() - 1)
+                throw std::runtime_error("CAN'T END EXPRESSION WITH <");
+            if (!tokens.size())
+                throw std::runtime_error("CAN'T START EXPRESSION WITH <");
+            if (tokens[tokens.size() - 1].type == RPAREN_TOKEN && tokens[tokens.size() - 1].type == STRING_TOKEN)
+                throw std::runtime_error("INCORRECT LEFT OPERAND FOR <");
+            if (input[i + 1] == '>')
+            {
+                i++;
+                if (i == tokens.size() - 1)
+                    throw std::runtime_error("CAN'T END EXPRESSION WITH <");
+                tokens.push_back({NEQ_OPERATOR_TOKEN, "NEQ OPERATOR", "<>"});
+            }
+            else if (input[i + 1] == '=')
+            {
+                i++;
+                if (i == tokens.size() - 1)
+                    throw std::runtime_error("CAN'T END EXPRESSION WITH <=");
+                tokens.push_back({LEQ_OPERATOR_TOKEN, "LEQ OPERATOR", "<="});
+            }
+            else
+            {
+                tokens.push_back({LESS_OPERATOR_TOKEN, "LESS OPERATOR", "<"});
+            }
+        }
+        else if (input[i] == '>')
+        {
+            if (i == tokens.size() - 1)
+                throw std::runtime_error("CAN'T END EXPRESSION WITH <");
+            if (!tokens.size())
+                throw std::runtime_error("CAN'T START EXPRESSION WITH <");
+            if (tokens[tokens.size() - 1].type == RPAREN_TOKEN && tokens[tokens.size() - 1].type == STRING_TOKEN)
+                throw std::runtime_error("INCORRECT LEFT OPERAND FOR <");
+            else if (input[i + 1] == '=')
+            {
+                i++;
+                if (i == tokens.size() - 1)
+                    throw std::runtime_error("CAN'T END EXPRESSION WITH >=");
+                tokens.push_back({GEQ_OPERATOR_TOKEN, "GEQ OPERATOR", ">="});
+            }
+            else
+            {
+                tokens.push_back({GREATER_OPERATOR_TOKEN, "GREATER OPERATOR", ">"});
+            }
         }
         else if (operators.find(input[i]) != std::string::npos)
         {
+            // /*^&"
             if (binary_math_operators.find(input[i]) != std::string::npos)
             {
                 if (!tokens.size())
                     throw std::runtime_error("BINARY OPERATOR MUST HAVE LEFT OPERAND");
-                if (tokens[tokens.size() - 1].type != NUMBER && tokens[tokens.size() - 1].type != REFERENCE && tokens[tokens.size() - 1].type != RPAREN)
+                if (tokens[tokens.size() - 1].type != NUMBER_TOKEN && tokens[tokens.size() - 1].type != REFERENCE_TOKEN && tokens[tokens.size() - 1].type != RPAREN_TOKEN)
                     throw std::runtime_error("INVALID LEFT OPERATOR FOR BINARY OPERAND");
                 std::string token;
                 token.push_back(input[i]);
-                tokens.push_back({OPERATOR, "OPERATOR", token});
+                switch (input[i])
+                {
+                case '/':
+                    tokens.push_back({DIV_OPERATOR_TOKEN, "DIV OPERATOR", token});
+                    break;
+                case '*':
+                    tokens.push_back({MULT_OPERATOR_TOKEN, "MULT OPERATOR", token});
+                    break;
+                case '^':
+                    tokens.push_back({POW_OPERATOR_TOKEN, "POW OPERATOR", token});
+                    break;
+                case '&':
+                    tokens.push_back({CONCAT_OPERATOR_TOKEN, "CONCAT OPERATOR", token});
+                    break;
+                }
             }
             else
             {
@@ -212,19 +487,22 @@ std::vector<Token> tokenize(std::string input)
                 {
                     if (!tokens.size())
                         throw std::runtime_error("CELL RANGE APPEND OPERATOR NEEDS LEFT OPERAND");
-                    if (tokens[tokens.size() - 1].type != REFERENCE)
-                        throw std::runtime_error("CELL RANGE APPEND OPERATOR NEEDS LEFT OPERAND (OF TYPE CELL REFERENCE)");
+                    if (tokens[tokens.size() - 1].type != REFERENCE_TOKEN && tokens[tokens.size() - 1].type != RPAREN_TOKEN)
+                        throw std::runtime_error("CELL RANGE APPEND OPERATOR NEEDS LEFT OPERAND (OF TYPE CELL REFERENCE_TOKEN OR EXPRESSION THAT RESOLVES TO CELL REFERENCE_TOKEN)");
                     std::string token;
                     token.push_back(input[i]);
-                    tokens.push_back({OPERATOR, "OPERATOR", token});
+                    tokens.push_back({RANGE_OPERATOR_TOKEN, "RANGE OPERATOR", token});
                 }
                 else if (input[i] == '+' || input[i] == '-')
                 {
-                    if (tokens.size() && tokens[tokens.size() - 1].type == STRING)
+                    if (tokens.size() && tokens[tokens.size() - 1].type == STRING_TOKEN)
                         throw std::runtime_error("STRING CANNOT BE FOLLOWED BY +/-");
                     std::string token;
                     token.push_back(input[i]);
-                    tokens.push_back({OPERATOR, "OPERATOR", token});
+                    if (input[i] == '+')
+                        tokens.push_back({ADD_OPERATOR_TOKEN, "ADD OPERATOR", token});
+                    else
+                        tokens.push_back({SUB_OPERATOR_TOKEN, "ADD OPERATOR", token});
                 }
             }
         }
@@ -256,14 +534,14 @@ std::vector<Token> tokenize(std::string input)
                         }
                         else if (valid_after_reference.find(input[i]) != std::string::npos)
                         {
-                            tokens.push_back({REFERENCE, "REFERENCE", value});
+                            tokens.push_back({REFERENCE_TOKEN, "REFERENCE_TOKEN", value});
                             i--;
                             should_break_outside = true;
                             token_pushed = true;
                             break;
                         }
                         else
-                            throw std::runtime_error("INVALID CELL REFERENCE");
+                            throw std::runtime_error("INVALID CELL REFERENCE_TOKEN");
                     }
                 }
                 else if (('a' <= input[i] && input[i] <= 'z') || ('A' <= input[i] && input[i] <= 'Z'))
@@ -278,7 +556,7 @@ std::vector<Token> tokenize(std::string input)
                     std::cout << "branch3\n";
                     if (valid_after_indentifier.find(input[i]) != std::string::npos)
                     {
-                        tokens.push_back({IDENT, "IDENT", value});
+                        tokens.push_back({IDENT_TOKEN, "IDENT", value});
                         i--;
                         token_pushed = true;
                         break;
@@ -294,11 +572,11 @@ std::vector<Token> tokenize(std::string input)
             {
                 if (is_cell_ref)
                 {
-                    tokens.push_back({REFERENCE, "REFERENCE", value});
+                    tokens.push_back({REFERENCE_TOKEN, "REFERENCE_TOKEN", value});
                 }
                 else
                 {
-                    tokens.push_back({IDENT, "IDENT", value});
+                    tokens.push_back({IDENT_TOKEN, "IDENT", value});
                 }
             }
         }
@@ -306,11 +584,52 @@ std::vector<Token> tokenize(std::string input)
     return tokens;
 }
 
+int operator_precendence(TOKEN_TYPE t)
+{
+    switch (t)
+    {
+    case PERCENT_OPERATOR_TOKEN:
+        return 100;
+    case POW_OPERATOR_TOKEN:
+        return 90;
+    case MULT_OPERATOR_TOKEN:
+        return 80;
+    case DIV_OPERATOR_TOKEN:
+        return 80;
+    case ADD_OPERATOR_TOKEN:
+        return 70;
+    case SUB_OPERATOR_TOKEN:
+        return 70;
+    case CONCAT_OPERATOR_TOKEN:
+        return 60;
+    case LESS_OPERATOR_TOKEN:
+        return 50;
+    case GREATER_OPERATOR_TOKEN:
+        return 50;
+    case GEQ_OPERATOR_TOKEN:
+        return 50;
+    case LEQ_OPERATOR_TOKEN:
+        return 50;
+    case EQ_OPERATOR_TOKEN:
+        return 50;
+    case NEQ_OPERATOR_TOKEN:
+        return 50;
+    case RANGE_OPERATOR_TOKEN:
+        return 40;
+    }
+    return 0;
+}
+
+ASTNode parse(std::vector<Token> &tokens)
+{
+}
+
 int main()
 {
-    std::vector<Token> tokens = tokenize("A1");
+    std::vector<Token> tokens = tokenize("INDIRECT(\"A1\"):B2>=5");
     for (int i = 0; i < tokens.size(); i++)
     {
         std::cout << tokens[i].token_type_string << " " << tokens[i].token << "\n";
     }
+    return 0;
 }
